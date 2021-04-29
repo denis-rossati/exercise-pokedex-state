53d342e6c47027a5faef51350c4a4da7497b0fd9
denis-rossati <denis.rossati@hotmail.com>
2021-04-29 18:53:41 -0300

    AEEEEEEEEe

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Pokemon from './Pokemon';
+import pokemons from './data';
+
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super(props)
+        this.nextPokemon = this.nextPokemon.bind(this);
+        this.state = {
+            indexPokemon: 0,
+        }
+    }
+
+    nextPokemon() {
+        if (this.state.indexPokemon === 8) {
+            this.setState( _ => ({ indexPokemon: 0 }))
+        }
+
+        this.setState((prevState) => ({ indexPokemon: prevState.indexPokemon + 1 }));
+    }
+
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                
+                <section className='pokemonCard'>
+                    <Pokemon pokemon={ pokemons[this.state.indexPokemon] } />
+                </section>
+                <br />
+                <button onClick={ this.nextPokemon }>Próximo</button>
             </div>
         );
     }
